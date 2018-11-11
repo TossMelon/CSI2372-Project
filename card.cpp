c91c48c104edd634a4848ff844c0a0dc7d78acc4
HuongP96 <44950151+HuongP96@users.noreply.github.com>
2018-11-11 14:41:25 -0500

    Update card.cpp

--- card.cpp
+++ card.cpp
@@ -12,8 +12,31 @@
 
 #include "card.h"
 
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
 //Card Constructor
 Card::Card( char faceIn, char colourIn) {
 	face = faceIn;
 	colour = colourIn;
+}
+
+//Returns a string for correspond rowNum of card
+string Card::row(int rowNum) {
+	if(rowNum == 0 || rowNum == 2) {
+		return colour<<colour<<colour;
+	} else {
+		return colour<<face<<colour;
+	}
+}
+
+//Print Card
+void Card::print() {
+	for(int row = 0; row<3; ++row) {
+		string rowString = c(row);
+		cout << rowString << endl;
+	}
 }
