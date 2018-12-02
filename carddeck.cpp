4138bfaa664d5b1ec7854d8f22b27fa75843993e
HuongP96 <44950151+HuongP96@users.noreply.github.com>
2018-12-02 00:21:29 -0500

    Update carddeck.cpp (DONE)

--- carddeck.cpp
+++ carddeck.cpp
@@ -13,48 +13,49 @@
 */
 
 #include "carddeck.h"
-#include "deck.cpp"
-#include "card.cpp"
+#include "deck.h"
+#include "card.h"
 
 CardDeck cDeck; //Create variable to hold card deck
 
-//Makes card deck with appropriate cards
+//If no card deck exists, make deck with appropriate cards
+//Else return existing deck
 CardDeck& CardDeck::make_CardDeck() {
 	if(cDeck.isEmpty()) {
 		//Create and add all 5 Crab cards to card deck
-		cDeck.addCard(Card(FaceAnimal::Crab,FaceBackground::Red));
-		cDeck.addCard(Card(FaceAnimal::Crab,FaceBackground::Green));
-		cDeck.addCard(Card(FaceAnimal::Crab,FaceBackground::Purple));
-		cDeck.addCard(Card(FaceAnimal::Crab,FaceBackground::Blue));
-		cDeck.addCard(Card(FaceAnimal::Crab,FaceBackground::Yellow));
+		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Red));
+		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Green));
+		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Purple));
+		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Blue));
+		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Yellow));
 		
 		//Create and add all 5 Penguin cards to card deck
-		cDeck.addCard(Card(FaceAnimal::Penguin,FaceBackground::Red));
-		cDeck.addCard(Card(FaceAnimal::Penguin,FaceBackground::Green));
-		cDeck.addCard(Card(FaceAnimal::Penguin,FaceBackground::Purple));
-		cDeck.addCard(Card(FaceAnimal::Penguin,FaceBackground::Blue));
-		cDeck.addCard(Card(FaceAnimal::Penguin,FaceBackground::Yellow));
+		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Red));
+		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Green));
+		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Purple));
+		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Blue));
+		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Yellow));
 		
 		//Create and add all 5 Octopus cards to card deck
-		cDeck.addCard(Card(FaceAnimal::Octopus,FaceBackground::Red));
-		cDeck.addCard(Card(FaceAnimal::Octopus,FaceBackground::Green));
-		cDeck.addCard(Card(FaceAnimal::Octopus,FaceBackground::Purple));
-		cDeck.addCard(Card(FaceAnimal::Octopus,FaceBackground::Blue));
-		cDeck.addCard(Card(FaceAnimal::Octopus,FaceBackground::Yellow));
+		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Red));
+		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Green));
+		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Purple));
+		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Blue));
+		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Yellow));
 		
 		//Create and add all 5 Turtle cards to card deck
-		cDeck.addCard(Card(FaceAnimal::Turtle,FaceBackground::Red));
-		cDeck.addCard(Card(FaceAnimal::Turtle,FaceBackground::Green));
-		cDeck.addCard(Card(FaceAnimal::Turtle,FaceBackground::Purple));
-		cDeck.addCard(Card(FaceAnimal::Turtle,FaceBackground::Blue));
-		cDeck.addCard(Card(FaceAnimal::Turtle,FaceBackground::Yellow));
+		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Red));
+		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Green));
+		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Purple));
+		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Blue));
+		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Yellow));
 		
 		//Create and add all 5 Walrus cards to card deck
-		cDeck.addCard(Card(FaceAnimal::Walrus,FaceBackground::Red));
-		cDeck.addCard(Card(FaceAnimal::Walrus,FaceBackground::Green));
-		cDeck.addCard(Card(FaceAnimal::Walrus,FaceBackground::Purple));
-		cDeck.addCard(Card(FaceAnimal::Walrus,FaceBackground::Blue));
-		cDeck.addCard(Card(FaceAnimal::Walrus,FaceBackground::Yellow));
+		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Red));
+		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Green));
+		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Purple));
+		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Blue));
+		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Yellow));
 		
 		//Shuffle card deck
 		cDeck.shuffle();
