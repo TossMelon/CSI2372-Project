a9714af03595b7d02e1b8f96a5e5782b7f28e6b9
HuongP96 <44950151+HuongP96@users.noreply.github.com>
2018-12-04 23:13:11 -0500

    Update rules.cpp (90% Done)

--- rules.cpp
+++ rules.cpp
@@ -1,11 +1,13 @@
-/* 
+/*
+	rules.cpp
+
 	CSI 2372 Project
 	Memory Card Game - Memoarrr!
-	
+
 	Group 36
 	Member 1: Huong Pham
 	Student Number: 7831858
-	
+
 	Member 2: Matthew Choi
 	Student Number: 8250803
 */
@@ -13,19 +15,20 @@
 #include "rules.h"
 
 Rules::Rules() {
-	
+	playerCounter = 1;
 }
 
 //Returns true if previous and current cards match, false otherwise
-bool isValid(const Game& g) {
-	if(g.getPreviousCard() == g.getCurrentCard()) {
+bool Rules::isValid(const Game& g) {
+	/*const Card* prev = g.getPreviousCard();
+	if() {
 		return true;
-	}	
+	}	*/
 	return false;
 }
 
 //Returns true if the number of rounds has reached 7
-bool gameOver(const Game& g) {
+bool Rules::gameOver(const Game& g) {
 	if(g.getRound() == 7) {
 		return true;
 	}
@@ -33,9 +36,41 @@ bool gameOver(const Game& g) {
 }
 
 //Return true if there is only one active Player left
-bool roundOver(const Game& g) {
-	if(g.getNextPlayer() == NULL) {
-		return true;
+bool Rules::roundOver(const Game& g) {
+	int activeCounter = 0;
+	
+	for(int i=0; i<3; i++) {
+		const Player& p = getNextPlayer(g);
+		if(p.isActive() == true) {
+		activeCounter++;
+		}
 	}
-	return false;
+	
+	playerCounter = 1;
+	
+	if(activeCounter == 1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//Return next player in the array of players
+const Player& Rules::getNextPlayer(const Game& g) {
+	if(playerCounter == 1) {
+		Player& p = g.getPlayer(Bottom);
+		playerCounter++;
+		return p;
+	} else if(playerCounter == 2) {
+		Player& p = g.getPlayer(Top);
+		playerCounter++;
+		return p;
+	} else if(playerCounter == 3) {
+		Player& p = g.getPlayer(Left);
+		playerCounter++;
+		return p;
+	} else {
+		Player& p = g.getPlayer(Right);
+		return p;
+	}
 }
