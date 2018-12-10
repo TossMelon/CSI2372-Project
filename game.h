a28ca82f5a069971695a757a9091d455b7b0749a
TossMelon <43581642+TossMelon@users.noreply.github.com>
2018-12-09 22:53:25 -0500

    Update game.h

--- game.h
+++ game.h
@@ -1,11 +1,11 @@
-/* 
+/*
 	CSI 2372 Project
 	Memory Card Game - Memoarrr!
-	
+
 	Group 36
 	Member 1: Huong Pham
 	Student Number: 7831858
-	
+
 	Member 2: Matthew Choi
 	Student Number: 8250803
 */
@@ -13,29 +13,38 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "board.h"
-#include "card.h"
+
 
 #include <iostream>
 
-//Aggregate
-class Player;
+#include "board.h"
+#include "card.h"
+#include "player.h"
+#include "carddeck.h"
 
 using std::ostream;
+
 
 class Game
 {
 	Board b;
-	int round;
+	int round, numPlayers;
+	Player *playerArray;
+	Card *cPrev, *cCurrent;
+	bool expertDisplay;
 	public:
 		Game(); //Game constructor
-		int getRound();
+		int getRound() const;
+		void nextRound(); //increments the number of rounds and resets the Game state for the next round
 		void addPlayer(const Player&);
-		Player& getNextPlayer();
-		const Card* getPreviousCard();
-		const Card* getCurrentCard();
-		void setCurrentCard(const Card*);
+		Player& getPlayer(Side) const;
+		Card* getPreviousCard() const;
+		Card* getCurrentCard() const;
+		void setCurrentCard(Card*);
+		Card* getCard(const Letter&, const Number&);
+		void setCard(const Letter&, const Number&, Card*);
 		friend ostream &operator << (ostream &out, const Game &g);
 };
 
 #endif
+
