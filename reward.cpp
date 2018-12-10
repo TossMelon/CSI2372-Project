0e9ab51d036dbeee067370e2b646e2451140621d
HuongP96 <44950151+HuongP96@users.noreply.github.com>
2018-12-09 20:30:48 -0500

    Update reward.cpp (FINAL)

--- reward.cpp
+++ reward.cpp
@@ -1,24 +1,31 @@
-/* 
+/*
 	reward.cpp
-	
+
 	CSI 2372 Project
 	Memory Card Game - Memoarrr!
-	
+
 	Group 36
 	Member 1: Huong Pham
 	Student Number: 7831858
-	
+
 	Member 2: Matthew Choi
 	Student Number: 8250803
 */
 
+//#define TEST_REWARD //Reward Test Compile Switch
+
+#include <iostream>
 #include "reward.h"
+
+using std::cout;
+using std::endl;
 
 //Reward constructor
 Reward::Reward(Rubies r) {
 	rubies = r;
 }
 
+//int conversion operator
 Reward::operator int() const {
 	switch(rubies) {
 		case Rubies::One:
@@ -34,6 +41,7 @@ Reward::operator int() const {
 			return 4;
 			break;
 	}
+	return 0;
 }
 
 //Overload stream insertion operator
@@ -41,3 +49,24 @@ Reward::operator int() const {
 ostream &operator << (ostream &out, const Reward &r) {
 	return out<<static_cast<int>(r);
 }
+
+//Reward Unit Test
+#ifdef TEST_REWARD
+#include "rewarddeck.h"
+
+int main() {
+	cout<<"Testing Reward Class"<<endl;
+
+	RewardDeck& deck = deck.make_RewardDeck();
+	Reward* r = deck.getNext();
+	int value = 0;
+	
+	while(r != nullptr) {
+		//The following two lines should match for each reward card
+		cout<<"\nostream output: "<<*r<<endl; //use overloaded ostream operator to display all reward cards of the deck
+		cout<<"int conversion output: "<<(int)*r<<endl; //use int conversion operator to display all reward cards of the deck
+		r = deck.getNext();
+	}
+}
+
+#endif
