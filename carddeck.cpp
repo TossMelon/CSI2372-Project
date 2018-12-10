c8eb58ff6c43fe0f66ed5503787e530fd4a921ef
HuongP96 <44950151+HuongP96@users.noreply.github.com>
2018-12-09 20:12:16 -0500

    Update carddeck.cpp (FINAL)

--- carddeck.cpp
+++ carddeck.cpp
@@ -1,64 +1,79 @@
-/* 
-	carddeck.cpp
-	
+/*
+	rewarddeck.cpp
+
 	CSI 2372 Project
 	Memory Card Game - Memoarrr!
-	
+
 	Group 36
 	Member 1: Huong Pham
 	Student Number: 7831858
-	
+
 	Member 2: Matthew Choi
 	Student Number: 8250803
 */
 
-#include "carddeck.h"
-#include "deck.h"
-#include "card.h"
+//#define TEST_REWARDDECK //RewardDeck Test Compile Switch
 
-CardDeck cDeck; //Create variable to hold card deck
+#include "rewarddeck.h"
 
-//If no card deck exists, make deck with appropriate cards
+RewardDeck rDeck; //Create variable to hold reward deck
+
+//If no reward deck exists, make deck with appropriate cards
 //Else return existing deck
-CardDeck& CardDeck::make_CardDeck() {
-	if(cDeck.isEmpty()) {
-		//Create and add all 5 Crab cards to card deck
-		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Red));
-		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Green));
-		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Purple));
-		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Blue));
-		cDeck.myvector.push_back(Card(FaceAnimal::Crab,FaceBackground::Yellow));
-		
-		//Create and add all 5 Penguin cards to card deck
-		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Red));
-		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Green));
-		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Purple));
-		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Blue));
-		cDeck.myvector.push_back(Card(FaceAnimal::Penguin,FaceBackground::Yellow));
-		
-		//Create and add all 5 Octopus cards to card deck
-		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Red));
-		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Green));
-		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Purple));
-		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Blue));
-		cDeck.myvector.push_back(Card(FaceAnimal::Octopus,FaceBackground::Yellow));
-		
-		//Create and add all 5 Turtle cards to card deck
-		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Red));
-		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Green));
-		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Purple));
-		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Blue));
-		cDeck.myvector.push_back(Card(FaceAnimal::Turtle,FaceBackground::Yellow));
-		
-		//Create and add all 5 Walrus cards to card deck
-		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Red));
-		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Green));
-		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Purple));
-		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Blue));
-		cDeck.myvector.push_back(Card(FaceAnimal::Walrus,FaceBackground::Yellow));
-		
-		//Shuffle card deck
-		cDeck.shuffle();
+RewardDeck& RewardDeck::make_RewardDeck() {
+	if(rDeck.isEmpty()) {
+		//Create and add all reward cards to reward deck
+		rDeck.myvector.push_back(Reward(Rubies::One));
+		rDeck.myvector.push_back(Reward(Rubies::One));
+		rDeck.myvector.push_back(Reward(Rubies::One));
+		rDeck.myvector.push_back(Reward(Rubies::Two));
+		rDeck.myvector.push_back(Reward(Rubies::Two));
+		rDeck.myvector.push_back(Reward(Rubies::Three));
+		rDeck.myvector.push_back(Reward(Rubies::Four));
+
+		//Shuffle reward deck
+		rDeck.shuffle();
 	}
-	return cDeck;
+	return rDeck;
 }
+
+//RewardDeck Unit Test
+#ifdef TEST_REWARDDECK
+
+int main() {
+	cout<<"Testing RewardDeck Class"<<endl;
+	
+	int numCards = 0;
+	int nOne = 0, nTwo = 0, nThree = 0, nFour = 0;
+	
+	cout<<"Make Reward Deck"<<endl;
+	RewardDeck& d = RewardDeck::make_RewardDeck();
+	cout<<"Print all Cards in Shuffled Reward Deck:\n"<<endl;
+	
+	Reward* r = d.getNext();
+	while(r != nullptr) {
+		cout<<*r<<endl;
+
+		if((int)*r == 1) {
+			nOne++;
+		} else if((int)*r == 2) {
+			nTwo++;
+		} else if((int)*r == 3) {
+			nThree++;
+		} else if((int)*r == 4) {
+			nFour++;
+		}
+		
+		numCards++;
+		r=d.getNext();
+	}
+	
+	cout<<"\nThis reward deck has a total of "<<numCards<<" cards\n";
+	cout<<"Number of Each Type of Reward Card:\n";
+	cout<<nOne<<" Cards with One Ruby\n";
+	cout<<nTwo<<" Cards with Two Rubies\n";
+	cout<<nThree<<" Cards with Three Rubies\n";
+	cout<<nFour<<" Cards with Four Rubies\n";
+}
+
+#endif
